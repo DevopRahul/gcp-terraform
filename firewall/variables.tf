52f94daa6719ae13deeac7e8111861c302a0bb5b
Rahul kushwaha <144446821+DevopRahul@users.noreply.github.com>
2025-08-10 15:51:45 +0530

    Create variables.tf

---
+++ firewall/variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  type = string
+  default = "qwiklabs-gcp-01-630ed67ed83c"
+}
+
+variable "bucket_name" {
+  type = string
+  default = "qwiklabs-gcp-01-630ed67ed83c-tf-state"
+}
+
+variable "region" {
+  type = string
+  default = "us-central1"
+}
